c990d6ed9ea098827a957ced4672382a7113448f
Jorge Botarro <chikinrasshu@icloud.com>
2024-12-21 11:08:18 -0300

    Fix stupid macro paste

--- include/chk/common/logging.h
+++ include/chk/common/logging.h
@@ -21,22 +21,22 @@ CHK_COMMON_API void chk_raw_log_f(chk_log_severity_t sev, const char *msg,
 
 #define chk_log(sev, msg, ...)                                                 \
     chk_raw_log##__VA_OPT__(_f)(sev, msg, __FILE__, __func__,                  \
-                                __LINE__##__VA_OPT__(, ) __VA_ARGS__)
+                                __LINE__ __VA_OPT__(, ) __VA_ARGS__)
 
 #define chk_dbg(msg, ...)                                                      \
-    chk_log(CHK_LOG_SEVERITY_DEBUG, msg##__VA_OPT__(, ) __VA_ARGS__)
+    chk_log(CHK_LOG_SEVERITY_DEBUG, msg __VA_OPT__(, ) __VA_ARGS__)
 
 #define chk_trace(msg, ...)                                                    \
-    chk_log(CHK_LOG_SEVERITY_TRACE, msg##__VA_OPT__(, ) __VA_ARGS__)
+    chk_log(CHK_LOG_SEVERITY_TRACE, msg __VA_OPT__(, ) __VA_ARGS__)
 
 #define chk_info(msg, ...)                                                     \
-    chk_log(CHK_LOG_SEVERITY_INFO, msg##__VA_OPT__(, ) __VA_ARGS__)
+    chk_log(CHK_LOG_SEVERITY_INFO, msg __VA_OPT__(, ) __VA_ARGS__)
 
 #define chk_warn(msg, ...)                                                     \
-    chk_log(CHK_LOG_SEVERITY_WARNING, msg##__VA_OPT__(, ) __VA_ARGS__)
+    chk_log(CHK_LOG_SEVERITY_WARNING, msg __VA_OPT__(, ) __VA_ARGS__)
 
 #define chk_error(msg, ...)                                                    \
-    chk_log(CHK_LOG_SEVERITY_ERROR, msg##__VA_OPT__(, ) __VA_ARGS__)
+    chk_log(CHK_LOG_SEVERITY_ERROR, msg __VA_OPT__(, ) __VA_ARGS__)
 
 #define chk_fatal(msg, ...)                                                    \
-    chk_log(CHK_LOG_SEVERITY_FATAL, msg##__VA_OPT__(, ) __VA_ARGS__)
+    chk_log(CHK_LOG_SEVERITY_FATAL, msg __VA_OPT__(, ) __VA_ARGS__)
